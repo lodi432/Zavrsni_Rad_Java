104f2ac429546d8ab2cf6c29594e9ccfcb7206c9
Korisnik <Korisnik@192.168.1.107>
2018-10-24 04:27:55 +0200

    InsertajSql za Proizvode

---
+++ pomocno/PhpCart/InsertajProizvode.sql
@@ -0,0 +1,7 @@
+INSERT INTO `proizvod` (`product_id`, `proizvodNaziv`, `product_image`, `proizvodOpis`, `product_price`) VALUES
+(1, 'Audi', 'care.jpg', 'Audi R8 je sportski model njemacke marke Audi', '1065679'),
+(2, 'Tesla S', 'stesla.jpg', 'Model S is the safest, quickest car on the road.', '1137820'),
+(3, 'Ferrari 458', 'ferrari458.jpg', 'Sports car produced by the Italian sports car manufacturer Ferrari.', '1750820'),
+(4, 'Rimac One', 'rimac.jpg', 'Supersportski elektricni automobil Concept One.', '5637820'),
+(5, 'Buggati Veyron', 'veyron.jpg', 'Buggati je supersportski automobil marke Bugatti kojeg proizvodi Volkswagen od 2005.', '9637820'),
+(6, 'YUGO 45', 'yugo.jpg', 'Yugo na solirani pogon.', '12637820');
